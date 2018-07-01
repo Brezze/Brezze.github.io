4d26357a3fdbe3992497d2d2dd5eae216b950685
Brezze <brezze@mail.ru>
2018-07-01 10:53:23 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,53 +1,16 @@
 $(function() {
 
-	$(window).on('resize', function() {
-    if ($(window).width() <= 992) {
-        $('.info-first').click(function(){
-				  $('.s-features__item-info + .first').toggleClass('open');
-				});
-				$('.info-second').click(function(){
-				  $('.s-features__item-info + .second').toggleClass('open');
-				});
-    } 
-}).trigger('resize');
-
-
-
-	// MODAL
-
-	$('.callback-modal').magnificPopup({
-	  type:'inline',
-	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-	});
-
-	// SLICK SLIDER
-
-	$('.s-plan-slider').slick({
-		dots: true,
-		arrows: false,
-		slidesToShow: 1,
-	});
-
-	// ACCORDION JQUERY
-
-	$('.s-document > li:eq(0) a').addClass('active').next().slideDown();
-
-    $('.s-document a').click(function(j) {
-        var dropDown = $(this).closest('li').find('.drop');
-
-        $(this).closest('.s-document').find('.drop').not(dropDown).slideUp();
-
-        if ($(this).hasClass('active')) {
-            $(this).removeClass('active');
-        } else {
-            $(this).closest('.s-document').find('a.active').removeClass('active');
-            $(this).addClass('active');
-        }
-
-        dropDown.stop(false, true).slideToggle();
-
-        j.preventDefault();
-    });
-
+	$('.btn-scroll').click(function(e) { // без мерцания
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#tren').position().top
+    }, 500);
+});
+	$('.btn-scroll1').click(function(e) { // без мерцания
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#form').position().top
+    }, 500);
 });
 
+});
