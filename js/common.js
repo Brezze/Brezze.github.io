7b1b35c7fb3db88df97632289c9fc661b4d59cda
Brezze <brezze@mail.ru>
2018-09-23 13:22:35 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,16 +1,37 @@
-$(function() {
+$(function(){
 
-	$('.btn-scroll').click(function(e) { // без мерцания
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#tren').position().top
-    }, 500);
-});
-	$('.btn-scroll1').click(function(e) { // без мерцания
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#form').position().top
-    }, 500);
-});
+	$('.inner-nav__toggler').click(function(){
+
+		$('.inner-nav__list').toggleClass('open')
+
+	})
+
+	$('.inner-search input').focusin(function(){
+
+		$('.inner-search__placeholder').fadeOut()
+
+	})
+
+	$('.inner-search input').focusout(function(){
+
+		$('.inner-search__placeholder').fadeIn()
+
+	})
 
 });
+
+document.querySelector("html").classList.add('js');
+
+var fileInput  = document.querySelector( ".input-file" ),  
+    button     = document.querySelector( ".input-file-trigger" ),
+    the_return = document.querySelector(".file-return");
+      
+button.addEventListener( "keydown", function( event ) {  
+    if ( event.keyCode == 13 || event.keyCode == 32 ) {  
+        fileInput.focus();  
+    }  
+});
+button.addEventListener( "click", function( event ) {
+   fileInput.focus();
+   return false;
+});  
