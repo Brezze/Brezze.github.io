ccde610dda24d92b16ab78f8351064be7eed36da
Brezze <brezze@mail.ru>
2018-11-21 10:14:07 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,92 +1,48 @@
 $(function() {
 
-    $('.clients-slider').slick({
-	  infinite: true,
-	  slidesToShow: 5,
-	  slidesToScroll: 1,
-	  rows: false,
-	  responsive: [
-	    {
-	      breakpoint: 1025,
-	      settings: {
-	        slidesToShow: 4,
-	      }
-	    },
-	    {
-	      breakpoint: 767,
-	      settings: {
-	        slidesToShow: 3,
-	      }
-	    },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        slidesToShow: 1,
-	      }
-	    }
-	    // You can unslick at a given breakpoint now by adding:
-	    // settings: "unslick"
-	    // instead of a settings object
-	  ]
-	});
+    $('.main-list a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('body').removeClass('hid');
+        $('.main-nav').removeClass('open');
+        $('.hamburger').removeClass('active')
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 
-    $('.propose-slider__item-content').first().show().animate({
-	    width: '40%',
-	  });
-	  $('.propose-slider__item-num').click(function() {
-	    $(this)
-	      .next().show().animate({
-	        width: '40%',
-	      })
-	      .siblings(".propose-slider__item-content").animate({
-	        width: 0
-	      });
+	$('.hamburger').click(function(){
+		$(this).toggleClass('active');
+		$('body').toggleClass('hid');
+		$('.main-nav').toggleClass('open');
+	})
 
-	  });
+    $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
 
-	$('.scroll[href*="#"]').on('click', function (e) {
-		e.preventDefault();
+    $('.accordion a').click(function(j) {
+        var dropDown = $(this).closest('li').find('.drop');
 
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top
-		}, 1100, 'linear');
-	});
+        $(this).closest('.accordion').find('.drop').not(dropDown).slideUp();
 
-	  $('.callbacks').magnificPopup({
-		  type:'inline',
-		  removalDelay: 500, //delay removal by X to allow out-animation
-		  callbacks: {
-		    beforeOpen: function() {
-		       this.st.mainClass = this.st.el.attr('data-effect');
-		    }
-		  },
-		  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-		});
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
 
-	  $('.map-item').magnificPopup({
-		  type:'inline',
-		  removalDelay: 500, //delay removal by X to allow out-animation
-		  callbacks: {
-		    beforeOpen: function() {
-		       this.st.mainClass = this.st.el.attr('data-effect');
-		    }
-		  },
-		  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-		});
+        dropDown.stop(false, true).slideToggle();
 
-	  $('.team-item__img-dots').magnificPopup({
-		  type:'inline',
-		  removalDelay: 500, //delay removal by X to allow out-animation
-		  callbacks: {
-		    beforeOpen: function() {
-		       this.st.mainClass = this.st.el.attr('data-effect');
-		    }
-		  },
-		  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-		});
+        j.preventDefault();
+    });
 
-	  $('.main-video__item').magnificPopup({
-		  type: 'iframe',
-		});
+    $('.main__slider').slick({
+    	rows: false,
+    	arrows: false,
+    	 autoplay: true,
+  		autoplaySpeed: 2000,
+  		fade: true,
+  		cssEase: 'linear',
+  		pauseOnHover: false,
+    })
 
 });
